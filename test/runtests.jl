2f1caf6ddcd219f6db92bc294395eb3076fd5d1f
SimonDanisch <sdanisch@gmail.com>
2022-06-01 18:02:36 +0200

    forgot save

--- test/runtests.jl
+++ test/runtests.jl
@@ -5,5 +5,4 @@ example_data = JLD2.load(joinpath(@__DIR__, "example.jld2"))
 pos = example_data["pos2"]
 data = example_data["data"]
 X, Y = pos[:,1],pos[:,2]
-times = range(-0.3000,stop=0.4980,step=1/500)
-TopoPlots.topoplot(Point2f.(X, Y), data[:,argmin(abs.(times .- .400)),1])
+TopoPlots.topoplot(Point2f.(X, Y), data[:,351,1], labels=string.(1:length(X)))
