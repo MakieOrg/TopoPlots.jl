e9206e39849a8ed0c7f5cd20974fe7b7ec3fd136
SimonDanisch <sdanisch@gmail.com>
2022-06-29 17:27:36 +0200

    better error for failed scipy install

--- deps/build.jl
+++ deps/build.jl
@@ -1,3 +1,9 @@
 @info "Installing scipy"
 using PyCall
-run(`$(PyCall.python) -m pip install scipy`)
+try
+    run(`$(PyCall.python) -m pip install scipy`)
+catch e
+    @warn("Couldn't install scipy in TopoPlots.jl build.jl.
+           Please make sure manually, that the python PyCall uses has scipy installed")
+    rethrow(e)
+end
