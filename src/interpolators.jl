223ff64544d214d873a549241a05f43c98176609
Anshul Singhvi <anshulsinghvi@gmail.com>
2024-04-23 15:35:27 -0400

    Warn against using DelaunayMesh in CairoMakie

--- src/interpolators.jl
+++ src/interpolators.jl
@@ -83,6 +83,11 @@ Really fast interpolation that happens on the GPU (for GLMakie), so optimal for 
 
 !!! warning
     `DelaunayMesh` won't allow you to add a contour plot to the topoplot.
+
+!!! danger
+    `DelaunayMesh` will not behave accurately if rendered via CairoMakie, because Cairo (and SVG in general)
+    does not support color maps on meshes.  The color within each triangle will be based only on the values 
+    at the vertices, which causes inaccurate visuals.
 """
 struct DelaunayMesh <: Interpolator
 end
